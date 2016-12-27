ebe756ff59299924f4824e6721e879a9827ae641
chisun-joung <csjoung@gmail.com>
2016-12-27 09:47:43 +0900

    inc dec remove counter

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ store.subscribe(render);
 render();
 
 const addCounter = (list) => {
-    return list.concat([0]);
+    return [...list, 0];
 
 };
 
@@ -69,5 +69,40 @@ const testAddCounter = () => {
     ).toEqual(listAfter);
 };
 
+const removeCounter = (list, index) => {
+    return [
+        ...list.slice(0, index),
+        ...list.slice(index + 1)
+    ];
+};
+
+const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+    deepfreeze(listBefore);
+    expect(
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+const incrementCounter = (list, index) => {
+    return [
+        ...list.slice(0,index),
+        list[index] + 1,
+        ...list.slice(index + 1)
+    ];
+};
+
+const testIncrementCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 11, 20];
+    deepfreeze(listBefore);
+    expect(
+        incrementCounter(listBefore, 1)
+    ).toEqual(listAfter)
+};
+
 testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
 console.log('All tests passed');
