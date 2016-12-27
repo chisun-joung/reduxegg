49776da9e0e1a0eafe2c4868cdb78a07e34eb60d
chisun-joung <csjoung@gmail.com>
2016-12-27 22:33:30 +0900

    15 combineReducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 const todo = (state, action) => {
     switch (action.type) {
@@ -48,18 +48,10 @@ action
     }
 };
 
-const todoApp = (state = {}, action) => {
-    return {
-        todos: todos(
-            state.todos,
-            action
-        ),
-        visibilityFilter: visibilityFilter(
-            state.visibilityFilter,
-            action
-        )
-    }
-}
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+})
 const store = createStore(todoApp);
 
 console.log('Initial state:');
