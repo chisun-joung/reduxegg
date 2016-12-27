4c97c668c33e11c068644abc871c8f0699c68547
chisun-joung <csjoung@gmail.com>
2016-12-27 10:19:07 +0900

    Writing a Todo List Reducer (Adding a Todo)

--- src/index.js
+++ src/index.js
@@ -73,6 +73,21 @@ const toggleTodo = (todo) => {
         completed: !todo.completed
     };
 };
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        default:
+            return state;
+    }
+};
 
 const testAddCounter = () => {
     const listBefore = [];
@@ -114,9 +129,29 @@ const testToggleTodo = () => {
         toggleTodo(todoBefore)
     ).toEqual(todoAfter);
 };
-
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+    deepfreeze(stateBefore);
+    deepfreeze(action);
+    expect(
+        todos(stateBefore,action)
+    ).toEqual(stateAfter);
+};
 testToggleTodo();
 testAddCounter();
 testRemoveCounter();
 testIncrementCounter();
+testAddTodo();
 console.log('All tests passed');
