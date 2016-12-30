56c599ea0d5a17dc61c48ffe80d23a82d1f5fc32
chisun-joung <csjoung@gmail.com>
2016-12-30 14:02:42 +0900

    container component FilterLink

--- src/index.js
+++ src/index.js
@@ -118,51 +118,75 @@ const Footer = ({
         {' '}
         <FilterLink
             filter="SHOW_ALL"
-            currentFilter={visibilityFilter}
-            onClick={onFilterClick}
         >
             ALL
         </FilterLink>
         {' '}
         <FilterLink
             filter="SHOW_ACTIVE"
-            currentFilter={visibilityFilter}
-            onClick={onFilterClick}
         >
             Active
         </FilterLink>
         {' '}
         <FilterLink
             filter="SHOW_COMPLETED"
-            currentFilter={visibilityFilter}
-            onClick={onFilterClick}
         >
             Completed
         </FilterLink>
     </p>
 )
 
-const FilterLink = ({
-    filter,
-    currentFilter,
+const Link = ({
+    active,
     children,
     onClick
 }) => {
-    if(filter === currentFilter){
-        return <span>{children}</span>
+    if(active) {
+        return <span>{children}</span>;
     }
-
     return (
-        <a href="#"
+        <a href='#'
            onClick={e => {
-           e.preventDefault();
-           onClick(filter);
+            e.preventDefault();
+            onClick();
            }}
         >
             {children}
         </a>
     );
 };
+
+class FilterLink extends React.Component {
+    componentDidMount(){
+        this.unsubscribe = store.subscribe(() => this.forceUpdate());
+    }
+    componentWillUnmount(){
+        this.unsubscribe();
+    }
+    render() {
+        const props = this.props;
+        const state = store.getState();
+
+        return(
+            <Link
+                active={
+                    props.filter ===
+                        state.visibilityFilter
+                }
+                onClick={() =>
+                    store.dispatch({
+                        type: 'SET_VISIBILITY_FILTER',
+                        filter: props.filter
+                    })
+                }
+            >
+                {props.children}
+            </Link>
+        );
+    }
+}
+
+
 
 const getVisibleTodos = (
     todos,
@@ -215,15 +239,7 @@ class TodoApp extends React.Component {
                         })
                     }
                 />
-                <Footer
-                    visibilityFilter={visibilityFilter}
-                    onFilterClick={filter =>
-                        store.dispatch({
-                            type: 'SET_VISIBILITY_FILTER',
-                            filter
-                        })
-                    }
-                />
+                <Footer/>
             </div>
         );
     }
@@ -241,4 +257,10 @@ const render = () => {
 
 store.subscribe(render);
 render();
+
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_ACTIVE'
+})
+console.log(store.getState());
 
